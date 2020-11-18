434460d382ec28d23673962da908dc0b020a04e4
liyuan <823260355@qq.com>
2020-11-18 20:55:25 +0800

    [liyuan] finish practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.ZoneOffset;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +12,15 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int month = 5;
+    int day = 1;
+    int timeStampToDayUnit = 1000 * 3600 * 24;
+    int year = date.getMonthValue() > month - 1 ? date.getYear() + 1 : date.getYear();
+    LocalDate targetDate = LocalDate.of(year, month, day);
+    long dateTimeStamp = date.atStartOfDay(ZoneOffset.ofHours(8)).toInstant().toEpochMilli();
+    long targetTimeStamp = targetDate.atStartOfDay(ZoneOffset.ofHours(8)).toInstant().toEpochMilli();
+    long dayTime = (targetTimeStamp - dateTimeStamp) / timeStampToDayUnit;
+
+    return dayTime;
   }
 }
